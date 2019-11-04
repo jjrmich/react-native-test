d32550307510865c6ef8b1bf6ddf043cb9f30a15
Joshua Richter <richte81@cse.msu.edu>
2019-11-04 16:34:03 -0500

    Made a lot of changes to CurvedRectangle component

--- js/RoundedRectScreen.js
+++ js/RoundedRectScreen.js
@@ -2,18 +2,29 @@
 
 import React, {Component} from 'react';
 import {
-  Platform,
+  TextInput,
   StyleSheet,
   StatusBar,
   Button,
   Text,
   View,
-  Alert,
+  InputAccessoryView,
+  Keyboard,
 } from 'react-native';
 import Svg from 'react-native-svg';
 import RoundedRect from './Components/RoundedRect';
 
 type Props = {};
+type State = {
+  x: number,
+  y: number,
+  height: number,
+  width: number,
+  curve: number,
+  stroke: string,
+  fill: string,
+  strokeWidth: number,
+};
 
 const styles = StyleSheet.create({
   testingContainer: {
@@ -22,39 +33,122 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: '5%',
   },
-  inputContainer: {
+  inputsContainer: {
     flex: 1,
     width: '100%',
+  },
+  inputContainer: {
+    flexDirection: 'row',
   },
   outputContainer: {
     flex: 2,
     backgroundColor: '#d3ebea',
     width: '100%',
   },
+  accessoryStyle: {
+    alignItems: 'flex-end',
+    paddingRight: '2.5%',
+    paddingTop: '1%',
+    paddingBottom: '1%',
+    backgroundColor: 'rgb(232, 236, 238)',
+  },
 });
 
-class RoundedRectScreen extends Component<Props> {
+class RoundedRectScreen extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      x: 0,
+      y: 0,
+      height: 0,
+      width: 0,
+      curve: 0,
+      stroke: 'transparent',
+      fill: 'orange',
+      strokeWidth: 0,
+    };
+  }
   render() {
+    const accessoryID = '001';
     return (
       <>
         <StatusBar barStyle="dark-content" />
         <View style={styles.testingContainer}>
-          <View style={styles.inputContainer}>
-            <Text>we out here</Text>
+          <View style={styles.inputsContainer}>
+            <View style={styles.inputContainer}>
+              <Text>X: </Text>
+              <TextInput
+                keyboardType="numeric"
+                onChangeText={text => this.setState({x: Number(text)})}
+                placeholder="0"
+                inputAccessoryViewID={accessoryID}
+                // value={this.state.x.toString()}
+                // returnKeyLabel="done"
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <Text>Y: </Text>
+              <TextInput
+                keyboardType="numeric"
+                onChangeText={text => this.setState({y: Number(text)})}
+                placeholder="0"
+                inputAccessoryViewID={accessoryID}
+                // value={this.state.x.toString()}
+                // returnKeyLabel="done"
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <Text>Height: </Text>
+              <TextInput
+                keyboardType="numeric"
+                onChangeText={text => this.setState({height: Number(text)})}
+                placeholder="0"
+                inputAccessoryViewID={accessoryID}
+                // value={this.state.x.toString()}
+                // returnKeyLabel="done"
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <Text>Width: </Text>
+              <TextInput
+                keyboardType="numeric"
+                onChangeText={text => this.setState({width: Number(text)})}
+                placeholder="0"
+                inputAccessoryViewID={accessoryID}
+                // value={this.state.x.toString()}
+                // returnKeyLabel="done"
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <Text>Curve: </Text>
+              <TextInput
+                keyboardType="numeric"
+                onChangeText={text => this.setState({curve: Number(text)})}
+                placeholder="0"
+                inputAccessoryViewID={accessoryID}
+                // value={this.state.x.toString()}
+                // returnKeyLabel="done"
+              />
+            </View>
           </View>
           <View style={styles.outputContainer}>
             <Svg width="100%" height="100%">
               <RoundedRect
-                x={12}
-                y={14}
-                height={40}
-                width={60}
-                curve={13}
-                fill="orange"
+                x={this.state.x}
+                y={this.state.y}
+                height={this.state.height}
+                width={this.state.width}
+                curve={this.state.curve}
+                fill={this.state.fill}
               />
             </Svg>
           </View>
         </View>
+        <InputAccessoryView nativeID={accessoryID}>
+          <View style={styles.accessoryStyle}>
+            <Button onPress={() => Keyboard.dismiss()} title="Done" />
+          </View>
+        </InputAccessoryView>
       </>
     );
   }
